4d410886d8b182d60b9e58c34761d1f7f57084a6
Aidos Mukatayev <53441647+mukatayev1@users.noreply.github.com>
2023-01-19 14:09:14 +0900

    Initial Commit

---
+++ SomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeApp
+//
+//  Created by Aidos Mukatayev on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
